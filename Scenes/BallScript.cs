a565495965b4b4c57db569a169d4d3d767548a96
Alexis Caasi <42218961+alexiscaasi@users.noreply.github.com>
2021-02-07 22:32:28 -0800

    Add files via upload

---
+++ Scenes/BallScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float speed = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      float xStart = Random.Range(0,2) == 0 ? -1 : 1;
+      float yStart = Random.Range(0,2) == 0 ? -1 : 1;
+
+      GetComponent<Rigidbody>().velocity = new Vector3 (speed * 2 * xStart, speed * 2 * yStart, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Scenes/ScoreScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreScript : MonoBehaviour
+{
+    public bool isGoal1;
+    public uint score1 = 0;
+    public uint score2 = 0;
+
+    void OnTriggerEnter(Collider other)
+        {
+            if(isGoal1){
+                score1 += 1;
+                Debug.Log($"Player2 Scored! Total Score: {score1} - {score2}");
+            }
+            else{
+                score2 += 2;
+                Debug.Log($"Player1 Scored! Total Score:{score2} - {score1}");
+            }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
